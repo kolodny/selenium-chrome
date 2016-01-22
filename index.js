0c6786ebde431d639272efd603751b1abfc47839
Moshe Kolodny <kolodny.github@gmail.com>
2016-01-22 00:54:47 -0500

    works with mocha

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ if (!isNode) {
 }
 
 Module._cache[module.filename] = undefined;
+var filename = process.platform === 'win32' ? 'chromedriver.exe' : 'chromedriver';
 
-exports = module.exports = function(callback) {
-  var filename = process.platform === 'win32' ? 'chromedriver.exe' : 'chromedriver';
+exports.before = function(next) {
   var localDriverLocation = path.join(path.resolve(path.dirname(module.parent)), filename);
+  ensureCachedDriverExists(function(error, cachedDriverLocation) {
+    if (error) { return next(error); }
+    fs.symlink(cachedDriverLocation, localDriverLocation, function(error) {
+      if (error && error.code === 'EEXIST') { return next(); }
+      next(error);
+    });
+  });
+
+
+exports.after = function(next) {
+  var localDriverLocation = path.join(path.resolve(path.dirname(module.parent)), filename);
+  fs.unlink(localDriverLocation, function(error) {
+    next(error);
+  })
+}
+
+function ensureCachedDriverExists(callback) {
+  var cachedDriverLocation = path.join(__dirname, filename);
+  if (fs.existsSync(cachedDriverLocation)) {
+    return callback(null, cachedDriverLocation);
+  }
   request(exports.URL + 'LATEST_RELEASE', function(error, latestRelease) {
     if (error) { return callback(error); }
     var zipUrl = {
@@ -26,7 +47,18 @@ exports = module.exports = function(callback) {
       .pipe(unzip.Parse())
       .on('entry', function (entry) {
         if (entry.path === filename) {
-          entry.pipe(fs.createWriteStream(localDriverLocation, { mode: 0o755 }));
+          var done = false;
+          entry.pipe(fs.createWriteStream(cachedDriverLocation + '_', { mode: 0o755 }));
+          entry.on('error', function(error) {
+            if (!done) { done = true; callback(error); }
+          })
+          entry.on('end', function() {
+            if (!done) {
+              done = true;
+              fs.renameSync(cachedDriverLocation + '_', cachedDriverLocation)
+              callback(null, cachedDriverLocation);
+            }
+          });
         } else {
           entry.autodrain();
         }
@@ -36,5 +68,3 @@ exports = module.exports = function(callback) {
 }
 
 exports.URL = 'http://chromedriver.storage.googleapis.com/';
-
-module.exports();
